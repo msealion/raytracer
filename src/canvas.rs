a80bc65a10d4175c9d1e90be6424a672b697deb1
msealion <ce.mattheus.cheong@gmail.com>
2023-01-12 22:16:50 +0800

    feat: add type CanvasSize

--- src/canvas.rs
+++ src/canvas.rs
@@ -1,6 +1,17 @@
 const PPM_HEADER: &str = "P3\n";
 const COLOUR_MAX: u64 = 255;
 
+struct CanvasSize {
+    width: u64,
+    height: u64,
+}
+
+impl CanvasSize {
+    pub fn new(width: u64, height: u64) -> CanvasSize {
+        CanvasSize { width, height }
+    }
+}
+
 struct Canvas {
     pixels: Vec<Vec<Colour>>,
 }
