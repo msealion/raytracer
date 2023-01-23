d346d01d14dbeb672fed40aaf2ad4571a2985cbe
msealion <ce.mattheus.cheong@gmail.com>
2023-01-23 12:27:16 +0800

    feat: add type PointLight

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 pub mod canvas;
 pub mod collections;
 pub mod intersections;
+pub mod light;
 pub mod objects;
 pub mod ray;
 pub mod transform;
---
+++ src/light.rs
@@ -0,0 +1,33 @@
+use crate::collections::*;
+
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub struct PointLight {
+    position: Point,
+    intensity: Colour,
+}
+
+impl PointLight {
+    pub fn new(position: Point, intensity: Colour) -> PointLight {
+        PointLight {
+            position,
+            intensity,
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn create_pointlight() {
+        let position = Point::new(1.0, 1.0, 1.0);
+        let intensity = Colour::new(0.0, 0.0, 0.0);
+        let pointlight = PointLight::new(position, intensity);
+        let resulting_pointlight = PointLight {
+            position,
+            intensity,
+        };
+        assert_eq!(pointlight, resulting_pointlight)
+    }
+}
