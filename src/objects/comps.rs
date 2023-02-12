8b08aacdb4548d413b3baa81b44c83f0c6272d93
msealion <ce.mattheus.cheong@gmail.com>
2023-02-12 14:21:03 +0800

    test: add test for Comps for ray inside shape

--- src/objects/comps.rs
+++ src/objects/comps.rs
@@ -10,6 +10,7 @@ pub(crate) struct Comps<'a> {
     point: Point,
     eyev: Vector,
     normalv: Vector,
+    inside: bool,
 }
 
 impl<'a> Comps<'a> {
@@ -18,13 +19,22 @@ impl<'a> Comps<'a> {
         let object = intersect.object();
         let point = ray.position(t);
         let eyev = -ray.direction;
-        let normalv = object.normal_at(point);
+        let mut normalv = object.normal_at(point);
+        let inside = match normalv.dot(eyev) {
+            _x if _x < 0.0 => {
+                normalv = -normalv;
+                true
+            }
+            _x if _x >= 0.0 => false,
+            _ => panic!(),
+        };
         Comps {
             t,
             object,
             point,
             eyev,
             normalv,
+            inside,
         }
     }
 }
@@ -35,7 +45,7 @@ mod tests {
     use crate::objects::{Intersect, Ray};
 
     #[test]
-    fn precompute_state() {
+    fn prepare_comps_ray_outside() {
         let ray = Ray::new(Point::new(0.0, 0.0, -5.0), Vector::new(0.0, 0.0, 1.0));
         let shape = Sphere::default();
         let intersect = Intersect::new(4.0, &shape);
@@ -45,7 +55,20 @@ mod tests {
             point: Point::new(0.0, 0.0, -1.0),
             eyev: Vector::new(0.0, 0.0, -1.0),
             normalv: Vector::new(0.0, 0.0, -1.0),
+            inside: false,
         };
         assert_eq!(Comps::prepare(&intersect, ray), resulting_comps);
+    }
+
+    #[test]
+    fn prepare_comps_ray_inside() {
+        let ray = Ray::new(Point::new(0.0, 0.0, 0.0), Vector::new(0.0, 0.0, 1.0));
+        let shape = Sphere::default();
+        let intersect = Intersect::new(1.0, &shape);
+        let prepared_comps = Comps::prepare(&intersect, ray);
+        assert_eq!(prepared_comps.point, Point::new(0.0, 0.0, 1.0));
+        assert_eq!(prepared_comps.eyev, Vector::new(0.0, 0.0, -1.0));
+        assert_eq!(prepared_comps.inside, true);
+        assert_eq!(prepared_comps.normalv, Vector::new(0.0, 0.0, -1.0));
     }
 }
